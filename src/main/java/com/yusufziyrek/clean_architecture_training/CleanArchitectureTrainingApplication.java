d620df06032a30f06b1a043f431f54017240a07c
Yusuf Ziyrek <yusufziyrek1@gmail.com>
2025-12-24 23:19:26 +0300

    project structure created

---
+++ src/main/java/com/yusufziyrek/clean_architecture_training/CleanArchitectureTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.yusufziyrek.clean_architecture_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusufziyrek/clean_architecture_training/CleanArchitectureTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufziyrek.clean_architecture_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
